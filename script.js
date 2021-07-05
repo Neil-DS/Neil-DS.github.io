2f2580b290ea4efd7304f1787c461a9d410b6474
Neil-DS <NeilDeSwart@gmail.com>
2021-07-05 18:41:20 +0100

    changed links, and added an image

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$('hyperlinked_row_reuse').onclick = function(){clicked_table_row('hyperlinked_row_reuse')};
-$('hyperlinked_row_brot').onclick = function(){clicked_table_row('hyperlinked_row_brot')};
+$('hyperlinked_row_1').onclick = function(){clicked_table_row('hyperlinked_row_1')};
+$('hyperlinked_row_2').onclick = function(){clicked_table_row('hyperlinked_row_2')};
 $('hyperlinked_row_3').onclick = function(){clicked_table_row('hyperlinked_row_3')};
 $('hyperlinked_row_4').onclick = function(){clicked_table_row('hyperlinked_row_4')};
 
