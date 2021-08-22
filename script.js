d8bd2fc8a7f47522e186dce4d6ba6c3b13d0f989
Neil-DS <NeilDeSwart@gmail.com>
2021-08-22 15:55:30 +0100

    fixed height overlapp

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ $('hyperlinked_row_2').onclick = function(){clicked_table_row('hyperlinked_row_2
 $('hyperlinked_row_3').onclick = function(){clicked_table_row('hyperlinked_row_3')};
 $('hyperlinked_row_4').onclick = function(){clicked_table_row('hyperlinked_row_4')};
 
-
 function $(x) {return document.getElementById(x);}
 
 function clicked_table_row(x){
     var link_urll = $(x);
-    window.location = link_urll.getAttribute("data-linker");
+   
+    window.location = link_urll.getAttribute("data-linker");   
 }
-
-
